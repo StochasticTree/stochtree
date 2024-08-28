2bcf35cc316c27ecbbbec51beee327e5e17778cf
Drew Herren <drewherrenopensource@gmail.com>
2024-08-28 16:46:11 -0500

    Update error variance calibration method to handle cases of rank deficiency and overdetermination

--- stochtree/__init__.py
+++ stochtree/__init__.py
@@ -1,5 +1,6 @@
 from .bart import BARTModel
 from .bcf import BCFModel
+from .calibration import calibrate_global_error_variance
 from .data import Dataset, Residual
 from .forest import ForestContainer
 from .preprocessing import CovariateTransformer
@@ -9,4 +10,4 @@ from .utils import NotSampledError
 
 __all__ = ['BARTModel', 'BCFModel', 'Dataset', 'Residual', 'ForestContainer', 
            'CovariateTransformer', 'RNG', 'ForestSampler', 'GlobalVarianceModel', 
-           'LeafVarianceModel', 'JSONSerializer', 'NotSampledError']
+           'LeafVarianceModel', 'JSONSerializer', 'NotSampledError', 'calibrate_global_error_variance']
