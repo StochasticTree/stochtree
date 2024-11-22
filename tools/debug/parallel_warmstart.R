fe15fc53fc5ae0913a34329e4aef9510bd489c68
Drew Herren <drewherrenopensource@gmail.com>
2024-11-22 17:33:58 -0600

    Updated parallel warmstart vignette

--- tools/debug/parallel_warmstart.R
+++ tools/debug/parallel_warmstart.R
@@ -4,25 +4,31 @@ library(foreach)
 library(doParallel)
 
 # Sampler settings
-num_chains <- 4
+num_chains <- 6
 num_gfr <- 10
 num_burnin <- 0
-num_mcmc <- 100
+num_mcmc <- 20
 num_trees <- 100
 
 # Generate the data
 n <- 500
-p_x <- 10
+p_x <- 100
 p_w <- 1
-snr <- 3
+snr <- 1
 X <- matrix(runif(n*p_x), ncol = p_x)
 W <- matrix(runif(n*p_w), ncol = p_w)
-f_XW <- (
-    ((0 <= X[,1]) & (0.25 > X[,1])) * (-7.5*W[,1]) + 
-        ((0.25 <= X[,1]) & (0.5 > X[,1])) * (-2.5*W[,1]) + 
-        ((0.5 <= X[,1]) & (0.75 > X[,1])) * (2.5*W[,1]) + 
-        ((0.75 <= X[,1]) & (1 > X[,1])) * (7.5*W[,1])
-)
+num_parts <- 20
+leaf_coefs <- seq(-10,10,length.out=num_parts*num_parts)
+dummy_mat_x1 <- model.matrix(~-1+cut(sin(2*pi*X[,1]), breaks = num_parts))
+dummy_mat_x2 <- model.matrix(~-1+cut(cos(2*pi*X[,2]), breaks = num_parts))
+dummy_mat <- matrix(0, nrow = n, ncol = num_parts*num_parts)
+for (i in 1:num_parts) {
+    for (j in 1:num_parts) {
+        dummy_mat[,(i-1)*num_parts + j] = dummy_mat_x1[,i]*dummy_mat_x2[,j]
+    }
+}
+leaf_transformations <- sapply(1:(num_parts*num_parts), function(i) W[,1]*leaf_coefs[i])
+f_XW <- rowSums(leaf_transformations * dummy_mat)
 noise_sd <- sd(f_XW) / snr
 y <- f_XW + rnorm(n, 0, 1)*noise_sd
 
@@ -46,6 +52,8 @@ xbart_model <- stochtree::bart(
     X_train = X_train, W_train = W_train, y_train = y_train, X_test = X_test, W_test = W_test, 
     num_gfr = num_gfr, num_burnin = 0, num_mcmc = 0, params = xbart_params
 )
+plot(rowMeans(xbart_model$y_hat_test), y_test); abline(0,1)
+cat(sqrt(mean((rowMeans(xbart_model$y_hat_test) - y_test)^2)), "\n")
 xbart_model_string <- stochtree::saveBARTModelToJsonString(xbart_model)
 
 # Parallel setup
@@ -100,5 +108,16 @@ for (i in 1:num_chains) {
          xlab = "predicted", ylab = "actual", 
          main = paste0("Chain ", i, "\nPredictions"))
     abline(0,1,col="red",lty=3,lwd=3)
+    cat(sqrt(mean((rowMeans(yhat_combined[,inds_start:inds_end]) - y_test)^2)), "\n")
 }
 par(mfrow = c(1,1))
+
+# Compare to a single chain of MCMC samples initialized at root
+bart_params <- list(sample_sigma_global = T, sample_sigma_leaf = T, 
+                    num_trees_mean = num_trees)
+bart_model <- stochtree::bart(
+    X_train = X_train, W_train = W_train, y_train = y_train, X_test = X_test, W_test = W_test, 
+    num_gfr = 0, num_burnin = 0, num_mcmc = num_mcmc, params = bart_params
+)
+plot(rowMeans(bart_model$y_hat_test), y_test, xlab = "predicted", ylab = "actual"); abline(0,1)
+cat(sqrt(mean((rowMeans(bart_model$y_hat_test) - y_test)^2)), "\n")
