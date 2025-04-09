d47ea41a7d080ec16ad8ea23ab5136e1ab95d1a0
Drew Herren <drewherrenopensource@gmail.com>
2025-04-09 18:06:22 -0500

    Updated build tools

--- setup.py
+++ setup.py
@@ -152,15 +152,8 @@ class CustomBuildPy(build_ext):
 
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
-__version__ = "0.1.0"
 
 setup(
-    name="stochtree",
-    version=__version__,
-    author="Drew Herren",
-    author_email="drewherrenopensource@gmail.com",
-    url="https://stochtree.ai/",
-    description="Stochastic Tree Ensembles for Machine Learning and Causal Inference",
     long_description="Stochastic Tree Ensembles for Machine Learning and Causal Inference",
     packages=find_packages(),
     ext_modules=[CMakeExtension("stochtree_cpp")],
@@ -171,22 +164,6 @@ setup(
     },
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
-    python_requires=">=3.8",
     license="MIT",
     license_files=["LICENSE.md", "CPP_DEPS_LICENSE.md"],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS", 
-        "Operating System :: Microsoft :: Windows", 
-        "Operating System :: POSIX :: Linux", 
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
 )
