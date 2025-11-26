f3d25e2431b932d0d87c5848349ef08ea7bd60a4
andrewherren <drewherrenopensource@gmail.com>
2025-11-26 05:28:05 +0000

    Deploying to r-dev from @ StochasticTree/stochtree@74a2c2545e1938e1d13b3c2ed6aa4fdded6421b6 🚀

--- R/bart.R
+++ R/bart.R
@@ -2219,6 +2219,11 @@ predict.bartmodel <- function(
       rfx_param_list <- object$rfx_samples$extract_parameter_samples()
       rfx_beta_draws <- rfx_param_list$beta_samples * y_std
 
+      # Promote to an array with consistent dimensions when there's one rfx term
+      if (length(dim(rfx_beta_draws)) == 2) {
+        dim(rfx_beta_draws) <- c(1, dim(rfx_beta_draws))
+      }
+
       # Construct a matrix with the appropriate group random effects arranged for each observation
       rfx_predictions_raw <- array(
         NA,
