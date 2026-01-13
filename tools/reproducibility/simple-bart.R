4c449142c9bac8029b745b17a98274504ff01c5f
Drew Herren <drewherrenopensource@gmail.com>
2026-01-12 18:19:50 -0600

    Update file paths for reproducibility check R script

--- tools/reproducibility/simple-bart.R
+++ tools/reproducibility/simple-bart.R
@@ -31,10 +31,10 @@ y_hat_in_sample <- predict(bart_model, X = X, type = "mean", terms = "y_hat")
 
 # Compare to stored results
 global_error_scale_trace_comparison <- as.numeric(read.csv(
-  "tools/replication/global_error_scale_trace.csv"
+  "tools/reproducibility/global_error_scale_trace.csv"
 )[, 1])
 y_hat_in_sample_comparison <- as.numeric(read.csv(
-  "tools/replication/y_hat_in_sample.csv"
+  "tools/reproducibility/y_hat_in_sample.csv"
 )[, 1])
 TOL <- 0.000001
 y_hat_mismatch <- !all(abs(y_hat_in_sample_comparison - y_hat_in_sample) < TOL)
