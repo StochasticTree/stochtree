674292eac74190703d3a306b9d4510bc4dc93336
Drew Herren <drewherrenopensource@gmail.com>
2026-03-04 16:09:16 -0600

    Added rhub workflow for containerized CRAN checks on GHA

--- cran-cleanup.R
+++ cran-cleanup.R
@@ -1,6 +1,6 @@
-# Create the stochtree_cran folder
+# Erase the stochtree_cran folder
 cran_dir <- "stochtree_cran"
 if (dir.exists(cran_dir)) {
-    # cran_subfolder_files <- list.files(cran_dir, recursive = TRUE, full.names = TRUE)
-    unlink(cran_dir, recursive = TRUE)
+  # cran_subfolder_files <- list.files(cran_dir, recursive = TRUE, full.names = TRUE)
+  unlink(cran_dir, recursive = TRUE)
 }
